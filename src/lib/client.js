5e6c29e49bad4d3b6ea579a73b47357439f3773b
Martin Rafael Gonzalez <tin@devtin.io>
2019-09-26 10:27:02 -0400

    add pleasure-api-client local

---
+++ src/lib/client.js
@@ -0,0 +1,13 @@
+import Cookies from 'js-cookie'
+import { PleasureApiClient } from 'pleasure-api-client'
+
+const clientPayload = {}
+
+console.log(`pleasure-ui-vue/client`)
+if (process.client && Cookies.get('accessToken')) {
+  // auto load accessToken
+  console.log(`auto loading access token`, Cookies.get('accessToken'))
+  clientPayload.accessToken = Cookies.get('accessToken')
+}
+
+export default PleasureApiClient.instance(clientPayload)
